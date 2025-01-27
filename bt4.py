b48e672ddf65a7fe113e152a33cc2264761e2ece
hongphucdinh <kitano0393@gmail.com>
2025-01-27 20:19:26 +0700

    bt4.py

---
+++ bt4.py
@@ -0,0 +1,13 @@
+a = int(input("nhap so tien:"))
+if a > 150:
+    tong= a -50
+    print(f"so tien can thanh toan la{tong}")
+elif a >100:
+    tong = a - 25
+    print(f"so tien can thanh toan la {tong}")
+elif a > 75:
+    tong = a - 15
+    print(f"so tien can thanh toan la{tong}")
+else:
+    tong = a 
+    print(f"so tien can thanh toan la {tong}")
